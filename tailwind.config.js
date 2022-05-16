04e6a6e8f8d4c949b238a1a3315d9bbd5fa3019f
yumna irwansyah <irwansyahfahril0808@gmail.com>
2022-05-17 01:43:28 +0700

    personal website v1

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['halamandepan.html'],
+  darkMode:'class',
+  theme: {
+    container: {
+        center: true,
+        padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary : '#14b8a6',
+        secondary: '#64748B',
+        dark : '#0f172a',
+      },
+      screen: {
+        '2xl' : '1320',
+      },
+    },
+  },
+  plugins: [],
+}
